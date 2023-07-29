4e9573a289810c07da6a15f2132f5f1427ff6970
BuildTools <unconfigured@null.spigotmc.org>
2023-07-29 16:36:23 +0200

    UPDATE

--- src/main/java/net/bernhardbmx/bbclient/BBClient.java
+++ src/main/java/net/bernhardbmx/bbclient/BBClient.java
@@ -6,6 +6,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
+
 @Mod(BBClient.MOD_ID)
 public class BBClient {
     public static final String MOD_ID = "bbclient";
